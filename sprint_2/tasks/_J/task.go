04f0031196bedbd08f07026dd110fb6b21d98f19
Dmitry Burov <dim0n1987@yandex.ru>
2022-03-25 23:09:03 +0300

    add task J

---
+++ sprint_2/tasks/_J/task.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type command struct {
+	action string
+	num    int
+}
+
+func main() {
+	n, commands, err := getInputData()
+	if err != nil {
+		showError(err)
+	}
+
+	// решение
+	result := solution(n, commands)
+	fmt.Println(strings.Join(result, "\n"))
+}
+
+func solution(n int, commands []command) (result []string) {
+	var stack []int
+
+	for i := 0; i < n; i++ {
+		switch commands[i].action {
+		case "get":
+			if len(stack) == 0 {
+				result = append(result, "error")
+			} else {
+				result = append(result, fmt.Sprint(stack[0]))
+				stack = stack[1:]
+			}
+			break
+		case "put":
+			stack = append(stack, commands[i].num)
+			break
+		case "size":
+			result = append(result, fmt.Sprint(len(stack)))
+			break
+		default:
+			break
+		}
+	}
+
+	return
+}
+
+// getInputData парсинг входных данных
+func getInputData() (n int, commands []command, err error) {
+
+	inp, err := getInputFromFile()
+	if err != nil {
+		return
+	}
+	defer func(inp *os.File) {
+		_ = inp.Close()
+	}(inp)
+
+	reader := bufio.NewReader(inp)
+
+	strNum, _, _ := reader.ReadLine()
+	n, err = strconv.Atoi(string(strNum))
+	if err != nil {
+		return
+	}
+
+	commands = make([]command, 0, n)
+
+	var num int
+	var com command
+	for i := 0; i < n; i++ {
+		strNums, _ := reader.ReadString('\n')
+		comStr := strings.Split(strings.TrimSpace(strNums), " ")
+
+		com = command{
+			action: comStr[0],
+		}
+		if len(comStr) == 2 {
+			num, _ = strconv.Atoi(comStr[1])
+			com.num = num
+		}
+
+		commands = append(commands, com)
+	}
+
+	defer reader.Reset(reader)
+	return
+}
+
+// getInputFromFile получение input из файла
+func getInputFromFile() (*os.File, error) {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	return file, nil
+}
+
+// showError вывод ошибки
+func showError(err error) {
+	panic(err)
+}
---
+++ sprint_2/tasks/_J/task_test.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+type task struct {
+	N      int
+	Input  []command
+	Result []string
+}
+
+func TestTask(t *testing.T) {
+
+	var taskItems = generateTasks()
+
+	for i := 0; i < len(taskItems); i++ {
+		fmt.Println(taskItems[i])
+		t.Run(fmt.Sprintf("Test %d", i+1), func(t *testing.T) {
+			res := solution(taskItems[i].N, taskItems[i].Input)
+			if strings.Join(res, "") != strings.Join(taskItems[i].Result, "") {
+				t.Errorf("Неверный ответ решения!\nОтвет: %s \nВерно: %s", res, taskItems[i].Result)
+			}
+		})
+	}
+}
+
+// generateTasks создает задачи для теста
+func generateTasks() (tasks []task) {
+	tasks = append(
+		tasks,
+		task{
+			N: 10,
+			Input: []command{
+				{
+					action: "put",
+					num:    -34,
+				},
+				{
+					action: "put",
+					num:    -23,
+				},
+				{
+					action: "get",
+				},
+				{
+					action: "size",
+				},
+				{
+					action: "get",
+				},
+				{
+					action: "size",
+				},
+				{
+					action: "get",
+				},
+				{
+					action: "get",
+				},
+				{
+					action: "put",
+					num:    80,
+				},
+				{
+					action: "size",
+				},
+			},
+			Result: []string{"-34", "1", "-23", "0", "error", "error", "1"},
+		},
+		task{
+			N: 6,
+			Input: []command{
+				{
+					action: "put",
+					num:    -66,
+				},
+				{
+					action: "put",
+					num:    98,
+				},
+				{
+					action: "size",
+				},
+				{
+					action: "size",
+				},
+				{
+					action: "get",
+				},
+				{
+					action: "get",
+				},
+			},
+			Result: []string{"2", "2", "-66", "98"},
+		},
+	)
+
+	return
+}
