486842d36cbb1a46fb531bbdc6618584e747ffc6
Dmitry Burov <dim0n1987@yandex.ru>
2022-04-09 21:03:07 +0300

    implement task A for sprint 3

--- sprint_3/tasks/A/task.go
+++ sprint_3/tasks/A/task.go
@@ -1,1 +1,24 @@
 package main
+
+import "fmt"
+
+func main() {
+
+	var n int
+	fmt.Scan(&n)
+
+	solution(n, "", 0, 0)
+}
+
+func solution(n int, line string, l, r int) {
+	if l == n && r == n {
+		fmt.Println(line)
+	} else {
+		if l < n {
+			solution(n, line+"(", l+1, r)
+		}
+		if r < l {
+			solution(n, line+")", l, r+1)
+		}
+	}
+}
--- sprint_3/tasks/A/task_test.go
+++ sprint_3/tasks/A/task_test.go
@@ -1,1 +1,51 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+type task struct {
+	N      int
+	Result []string
+}
+
+func TestTask(t *testing.T) {
+
+	var taskItems = generateTasks()
+
+	for i := 0; i < len(taskItems); i++ {
+		t.Run(fmt.Sprintf("Test %d", i+1), func(t *testing.T) {
+			res := solution(taskItems[i].N, "", 0, 0)
+			if fmt.Sprint(res) != fmt.Sprint(taskItems[i].Result) {
+				t.Errorf("Неверный ответ решения!\nОтвет: %s \nВерно: %s", res, taskItems[i].Result)
+			}
+		})
+	}
+}
+
+// generateTasks создает задачи для теста
+func generateTasks() (tasks []task) {
+	tasks = append(
+		tasks,
+		task{
+			N: 3,
+			Result: []string{
+				"((()))",
+				"(()())",
+				"(())()",
+				"()(())",
+				"()()()",
+			},
+		},
+		task{
+			N: 2,
+			Result: []string{
+				"(())",
+				"()()",
+			},
+		},
+	)
+
+	return
+}
