97ac9444d6aa10f7193deb46827dbeeb84c29e4b
Dmitry Burov <dim0n1987@yandex.ru>
2022-04-14 18:08:57 +0300

    task L not complete!

--- sprint_3/tasks/L/task.go
+++ sprint_3/tasks/L/task.go
@@ -1,1 +1,99 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	n, price, days, err := getInputData()
+	if err != nil {
+		showError(err)
+	}
+
+	fmt.Println(strings.Trim(fmt.Sprint(findDays(n, price, days)), "[]"))
+}
+
+func findDays(n, price int, days []int) []int {
+	var day1, day2 int
+
+	day1 = binarySearch(price, days, 0, n)
+	if day1 > 0 {
+		day2 = binarySearch(price*2, days, day1, n)
+	} else {
+		day2 = -1
+	}
+
+	return []int{day1, day2}
+}
+
+func binarySearch(x int, arr []int, l, r int) int {
+	if r <= l {
+		return -1
+	}
+
+	m := l + (r-l)/2
+	fmt.Println(l, r, m)
+	if arr[m] == x {
+		return m
+	} else if arr[m] > x {
+		return binarySearch(x, arr, l, m)
+	} else {
+		return binarySearch(x, arr, m+1, r)
+	}
+}
+
+func getInputData() (n, price int, daysPrice []int, err error) {
+
+	input, err := getInputFromFile()
+	if err != nil {
+		showError(err)
+	}
+	// close file
+	defer func(input *os.File) {
+		_ = input.Close()
+	}(input)
+
+	reader := bufio.NewReader(input)
+
+	strNum, _, _ := reader.ReadLine()
+	n, err = strconv.Atoi(string(strNum))
+	if err != nil {
+		return
+	}
+
+	var num int
+	daysPrice = make([]int, n)
+	strNums, _ := reader.ReadString('\n')
+	strNumsArr := strings.Split(strings.TrimSpace(strNums), " ")
+	for i := 0; i < n; i++ {
+		num, _ = strconv.Atoi(strNumsArr[i])
+		daysPrice[i] = num
+	}
+
+	strNum, _, _ = reader.ReadLine()
+	price, err = strconv.Atoi(string(strNum))
+	if err != nil {
+		return
+	}
+
+	// clear bufio
+	defer reader.Reset(reader)
+	return
+}
+
+func getInputFromFile() (*os.File, error) {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	return file, nil
+}
+
+func showError(err interface{}) {
+	panic(err)
+}
--- sprint_3/tasks/L/task_test.go
+++ sprint_3/tasks/L/task_test.go
@@ -1,1 +1,61 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+type task struct {
+	N, X   int
+	Days   []int
+	Result []int
+}
+
+func TestTask(t *testing.T) {
+
+	var taskItems = generateTasks()
+
+	for i := 0; i < len(taskItems); i++ {
+		t.Run(fmt.Sprintf("Test %d", i+1), func(t *testing.T) {
+			res := findDays(taskItems[i].N, taskItems[i].X, taskItems[i].Days)
+			if fmt.Sprint(res) != fmt.Sprint(taskItems[i].Result) {
+				t.Errorf("Неверный ответ решения!\nОтвет: %v \nВерно: %v", res, taskItems[i].Result)
+			} else {
+				t.Log("OK")
+			}
+		})
+	}
+}
+
+// generateTasks создает задачи для теста
+func generateTasks() (tasks []task) {
+	tasks = append(
+		tasks,
+		task{
+			N:      6,
+			X:      10,
+			Days:   []int{1, 2, 4, 4, 4, 4},
+			Result: []int{-1, -1},
+		},
+		task{
+			N:      7,
+			X:      3,
+			Days:   []int{1, 1, 4, 4, 4, 4, 8},
+			Result: []int{3, 7},
+		},
+		task{
+			N:      6,
+			X:      1,
+			Days:   []int{1, 2, 4, 4, 4, 4},
+			Result: []int{1, 2},
+		},
+		task{
+			N:      17,
+			X:      28,
+			Days:   []int{12, 19, 29, 31, 33, 37, 52, 62, 66, 68, 71, 75, 75, 88, 90, 93, 98},
+			Result: []int{3, 8},
+		},
+	)
+
+	return
+}
