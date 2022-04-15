910e20af63a1a60724b9eb69f3722f80dce55d94
Dmitry Burov <dim0n1987@yandex.ru>
2022-04-15 15:17:08 +0300

    complete B task

--- sprint_3/tasks/B/task.go
+++ sprint_3/tasks/B/task.go
@@ -1,1 +1,48 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+var numberRules = map[string]string{
+	"2": "abc",
+	"3": "def",
+	"4": "ghi",
+	"5": "jkl",
+	"6": "mno",
+	"7": "pqrs",
+	"8": "tuv",
+	"9": "wxyz",
+}
+
+func main() {
+	var input string
+	_, _ = fmt.Scan(&input)
+
+	fmt.Println(strings.Trim(fmt.Sprint(solution(input)), "[]"))
+}
+
+func solution(digits string) []string {
+	if digits == "" {
+		return nil
+	}
+
+	var result []string
+	var words = numberRules[digits[len(digits)-1:]]
+	var combinations = solution(digits[:len(digits)-1])
+
+	if len(combinations) > 0 {
+		for _, combination := range combinations {
+			for _, c := range strings.Split(words, "") {
+				result = append(result, combination+c)
+			}
+		}
+	} else {
+		for _, c := range strings.Split(words, "") {
+			result = append(result, c)
+		}
+	}
+
+	return result
+}
--- sprint_3/tasks/B/task_test.go
+++ sprint_3/tasks/B/task_test.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+type task struct {
+	Input  string
+	Result string
+}
+
+func TestTask(t *testing.T) {
+	var taskItems = generateTasks()
+	for i := 0; i < len(taskItems); i++ {
+		t.Run(fmt.Sprintf("Test %d", i+1), func(t *testing.T) {
+			res := solution(taskItems[i].Input)
+			if strings.Trim(fmt.Sprint(res), "[]") != taskItems[i].Result {
+				t.Errorf("Неверный ответ решения!\nОтвет: %s \nВерно: %s", res, taskItems[i].Result)
+			}
+		})
+	}
+}
+
+func generateTasks() (tasks []task) {
+	tasks = append(
+		tasks,
+		task{
+			Input:  "23",
+			Result: "ad ae af bd be bf cd ce cf",
+		},
+		task{
+			Input:  "92",
+			Result: "wa wb wc xa xb xc ya yb yc za zb zc",
+		},
+	)
+
+	return
+}
