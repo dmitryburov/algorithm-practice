f5e2ec91fe7f2d32279972cb8edff8e5af26b03a
Dmitry Burov <dim0n1987@yandex.ru>
2022-04-15 16:49:07 +0300

    not completed task C

--- sprint_3/tasks/C/task.go
+++ sprint_3/tasks/C/task.go
@@ -1,1 +1,84 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	str1, str2, err := getInputData()
+	if err != nil {
+		showError(err)
+	}
+
+	fmt.Println(solution(strings.TrimSpace(str1), strings.TrimSpace(str2)))
+}
+
+func solution(s, t string) string {
+	lenS, lenT := len(s), len(t)
+
+	if lenS == 0 {
+		return "True"
+	}
+	if lenS > lenT {
+		return "False"
+	}
+
+	for i, j := 0, 0; j < lenT; j++ {
+		if s[i] == t[j] {
+			i++
+			if i == lenS {
+				return "True"
+			}
+		}
+	}
+
+	return "False"
+}
+
+func getInputData() (s, t string, err error) {
+	var input *os.File
+	var btStr []byte
+
+	input, err = getInputFromFile()
+	if err != nil {
+		showError(err)
+	}
+	// close file
+	defer func(input *os.File) {
+		_ = input.Close()
+	}(input)
+
+	reader := bufio.NewReader(input)
+
+	btStr, _, err = reader.ReadLine()
+	if err != nil {
+		return
+	}
+	s = string(btStr)
+
+	btStr, _, err = reader.ReadLine()
+	if err != nil {
+		return
+	}
+	t = string(btStr)
+
+	// clear bufio
+	defer reader.Reset(reader)
+	return
+}
+
+func getInputFromFile() (*os.File, error) {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	return file, nil
+}
+
+func showError(err interface{}) {
+	panic(err)
+}
--- sprint_3/tasks/C/task_test.go
+++ sprint_3/tasks/C/task_test.go
@@ -1,1 +1,59 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+type task struct {
+	Str1, Str2 string
+	Result     string
+}
+
+func TestTask(t *testing.T) {
+	var taskItems = generateTask()
+	for i := 0; i < len(taskItems); i++ {
+		t.Run(fmt.Sprintf("Test %d", i+1), func(t *testing.T) {
+			res := solution(taskItems[i].Str1, taskItems[i].Str2)
+			if res != taskItems[i].Result {
+				t.Errorf("Неверный ответ решения!\nОтвет: %s \nВерно: %s", res, taskItems[i].Result)
+			}
+		})
+	}
+
+}
+
+func generateTask() []task {
+	return []task{
+		{
+			Str1:   "abcp",
+			Str2:   "ahpc",
+			Result: "False",
+		},
+		{
+			Str1:   "abc",
+			Str2:   "ahbgdcu",
+			Result: "True",
+		},
+		{
+			Str1:   "islx",
+			Str2:   "yoytgtshldmogkdburkbcfvoapepjpcuwemusfkfztrzxstytrnarlizjhuoscuzlraezlaweipuuqdgvhwkhhoufexojaps",
+			Result: "True",
+		},
+		{
+			Str1:   "ijha",
+			Str2:   "hmrqvftefyixinahlzgbkidroxiptbbkjmtwpsujevkulgrjiwiwzyhngulrodiwyg",
+			Result: "False",
+		},
+		{
+			Str1:   "m",
+			Str2:   "zzkqxfdxbbjqhatygmtmpgbhumicrhtjkrfblwwnjlebsfdawznznxwyzehpubvdukmgwrivygosfkdquwxvkvtfvwjfwtvjvtplpckktmnhfnxprjetpxnddoiiqotzrjjfdwnzdjvtclcqwsvsegnuajookwppzfsf",
+			Result: "True",
+		},
+		{
+			Str1:   " ",
+			Str2:   "ggzjxvsyleybxdjtmrfdvvuiwhamerqnavroixmxkyirnktbfsyijmtqmapjwuttpwefgfdtisrmzpfalwgefvbeecbdcllvfkylplpjmqquqhmjsplpmancofucoymnbtoitltfhuolrgzlwikinojtjvhgeszkmitqgslrvmfyuxnlkzhguvfqxfbknvmxflyxuxcrgwnhbonuqjvmzmzfdapsukwqqdzpicbnhrntgdqxnajwrrgjqlncuezesuzpscjtihrfadapwslrflbtsnvismrhpjfjqvgc",
+			Result: "True",
+		},
+	}
+}
