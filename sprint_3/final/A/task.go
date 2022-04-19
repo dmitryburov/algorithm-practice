1f0bc9e2db10adbaebf4d5aec9cd032f84419d0a
Dmitry Burov <dim0n1987@yandex.ru>
2022-04-19 14:47:30 +0300

    update final tasks `sprint 3`

--- sprint_3/final/A/task.go
+++ sprint_3/final/A/task.go
@@ -1,24 +1,5 @@
 package main
 
-/*
-ID посылки: 67488184
-
-ПРИНЦИП РАБОТЫ ---
-Алгоритм работает по принципу бинарного поиска (за O(log n)) с ипользованием рекурсии.
-При определении нужной стороны мы задаем промежуток для рекурсии далее
-Добавил комментарии дополнительно в коде.
-
-ВРЕМЕННАЯ СЛОЖНОСТЬ --
-Алгоритм работает за О(log n), тк мы проходим только по нужной части
-массива, пока не найдем К или не закончится его "деление"
-
-ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --
-Массив сожержащий n элементов занимает O(n) памяти
-Мы работаем в рамках памяти для выделенного массива из n элементов
-Дополнительно выделяется стековая память для рекурсивных вызовов.
-
-*/
-
 // brokenSearch основная функция
 func brokenSearch(arr []int, k int) int {
 	return binarySearch(arr, k, 0, len(arr)-1)
@@ -27,45 +8,31 @@ func brokenSearch(arr []int, k int) int {
 // binarySearch бинарный поиск по массиву
 func binarySearch(arr []int, k, left, right int) int {
 
-	mid := (left + right + 1) / 2
-
-	// базовые случаи
-	if arr[mid] == k {
-		return mid
-	}
-	if left == right {
+	if left > right {
 		return -1
 	}
 
-	// рекурсивные случаи
-	lf, rg := left, mid-1
-	for {
-		// если слева ок
-		if arr[0] < arr[mid] {
-			if k >= arr[0] {
-				if k > arr[mid] {
-					lf, rg = mid, right
-					break
-				}
-				break
-			}
-
-			lf, rg = mid, right
-			break
-		}
-
-		// если сломан слева
-		if k < arr[0] {
-			if k < arr[mid] {
-				break
-			}
-			lf, rg = mid, right
-			break
-		}
-
-		// один проход
-		break
+	mid := (right + left) / 2
+	if arr[mid] == k {
+		return mid
 	}
 
-	return binarySearch(arr, k, lf, rg)
+	// если левая часть отсортирована
+	if arr[left] <= arr[mid] {
+		if k >= arr[left] && k <= arr[mid] {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+		// тогда по правой стороне
+	} else {
+		if k >= arr[mid] && k <= arr[right] {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	// рекурсивно идем дальше с учетём измененного отрезка
+	return binarySearch(arr, k, left, right)
 }
