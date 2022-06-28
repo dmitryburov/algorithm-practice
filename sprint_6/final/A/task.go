6ab1833cf42eaa88344e5247c3d36f52dfcdfe6b
Dmitry Burov <dim0n1987@yandex.ru>
2022-06-28 23:23:18 +0300

    update home

--- sprint_6/final/A/task.go
+++ sprint_6/final/A/task.go
@@ -23,7 +23,6 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"log"
 	"os"
 	"strconv"
 	"strings"
@@ -39,22 +38,62 @@ func main() {
 }
 
 // Solution решение задачи
-func Solution(r io.Reader, w *strings.Builder) {
-	var err error
+func Solution(r io.Reader, s *strings.Builder) {
 	scanner := bufio.NewScanner(r)
 	scanner.Scan()
 
 	// количество вершин и ребер
 	var n, m int
 	nmData := strings.Fields(scanner.Text())
-	n, err = strconv.Atoi(nmData[0])
-	if err != nil {
-		log.Fatal(err)
-	}
-	m, err = strconv.Atoi(nmData[1])
-	if err != nil {
-		log.Fatal(err)
+	n, _ = strconv.Atoi(nmData[0])
+	m, _ = strconv.Atoi(nmData[1])
+
+	// fix empty
+	if n > 1 && m == 0 {
+		s.WriteString(ErrOops)
+		return
 	}
 
-	w.WriteString(ErrOops)
+	edgesList := make([][]int, n)
+
+	var v, u, w int
+	for i := 1; i <= m; i++ {
+		scanner.Scan()
+		edgeData := strings.Fields(scanner.Text())
+		v, _ = strconv.Atoi(edgeData[0])
+		u, _ = strconv.Atoi(edgeData[1])
+		m, _ = strconv.Atoi(edgeData[2])
+
+		// fix если равны
+		if v == u {
+			continue
+		}
+
+		//if _, ok := edgesList[u][v]; !ok || (ok && edgesList[u][v] < w) {
+		//	edgesList[v][u] = w
+		//	edgesList[u][v] = w
+		//}
+	}
+
+	var mst int
+	var edges []int
+
+	res, err := FindMST(edgesList, edges, mst)
+	if err != nil {
+		s.WriteString(ErrOops)
+		return
+	}
+
+	s.WriteString(fmt.Sprint(res))
+}
+
+// FindMST поиск минимального остовного дерева
+func FindMST(graph [][]int, edges []int, mst int) (int, error) {
+
+	return 0, nil
+}
+
+// AddPeak добавление вершины
+func AddPeak() {
+
 }
