4955e6ed4ccf896a8e5114e3c863e4b04a833e20
Dmitry Burov <dim0n1987@yandex.ru>
2022-06-30 22:08:58 +0300

    update task A sprint 3

--- sprint_3/tasks/A/task.go
+++ sprint_3/tasks/A/task.go
@@ -1,24 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 
 	var n int
 	fmt.Scan(&n)
 
-	solution(n, "", 0, 0)
+	res := strings.Builder{}
+	solution(n, "", 0, 0, &res)
+	fmt.Println(strings.TrimRight(res.String(), "\n"))
 }
 
-func solution(n int, line string, l, r int) {
+func solution(n int, line string, l, r int, s *strings.Builder) {
 	if l == n && r == n {
-		fmt.Println(line)
+		s.WriteString(line + "\n")
 	} else {
 		if l < n {
-			solution(n, line+"(", l+1, r)
+			solution(n, line+"(", l+1, r, s)
 		}
 		if r < l {
-			solution(n, line+")", l, r+1)
+			solution(n, line+")", l, r+1, s)
 		}
 	}
 }
--- sprint_3/tasks/A/task_test.go
+++ sprint_3/tasks/A/task_test.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 )
 
 type task struct {
 	N      int
-	Result []string
+	Result string
 }
 
 func TestTask(t *testing.T) {
@@ -16,35 +17,29 @@ func TestTask(t *testing.T) {
 
 	for i := 0; i < len(taskItems); i++ {
 		t.Run(fmt.Sprintf("Test %d", i+1), func(t *testing.T) {
-			res := solution(taskItems[i].N, "", 0, 0)
-			if fmt.Sprint(res) != fmt.Sprint(taskItems[i].Result) {
-				t.Errorf("Неверный ответ решения!\nОтвет: %s \nВерно: %s", res, taskItems[i].Result)
+			s := strings.Builder{}
+			solution(taskItems[i].N, "", 0, 0, &s)
+			if strings.TrimRight(s.String(), "\n") != taskItems[i].Result {
+				t.Errorf("Неверный ответ решения!\nОтвет: %s \nВерно: %s", s.String(), taskItems[i].Result)
 			}
 		})
 	}
 }
 
-// generateTasks создает задачи для теста
 func generateTasks() (tasks []task) {
 	tasks = append(
 		tasks,
 		task{
 			N: 3,
-			Result: []string{
-				"((()))",
-				"(()())",
-				"(())()",
-				"()(())",
-				"()()()",
-			},
-		},
+			Result: `((()))
+(()())
+(())()
+()(())
+()()()`},
 		task{
 			N: 2,
-			Result: []string{
-				"(())",
-				"()()",
-			},
-		},
+			Result: `(())
+()()`},
 	)
 
 	return
