2204b1f346401a51b892d341218f6d233f29377f
Dmitry Burov <dim0n1987@yandex.ru>
2022-07-01 18:16:29 +0300

    skeleton test & task sprint 7 final task B

--- sprint_7/final/B/task.go
+++ sprint_7/final/B/task.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"io"
+	"strings"
+)
+
+func main() {
+
+}
+
+func Solution(r io.Reader, w *strings.Builder) {
+
+}
--- sprint_7/final/B/task_test.go
+++ sprint_7/final/B/task_test.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"strings"
+	"testing"
+)
+
+type TestItem struct {
+	input  io.Reader
+	output string
+}
+
+func TestTask(t *testing.T) {
+	tasks := generateTasks()
+
+	for i, v := range tasks {
+		t.Run(fmt.Sprintf("Test %d", i+1), func(t *testing.T) {
+			buf := strings.Builder{}
+			Solution(v.input, &buf)
+			if buf.String() != v.output {
+				t.Errorf("Неверный ответ решения!\nОтвет: \n%v \nВерно: \n%v", buf.String(), v.output)
+			}
+		})
+	}
+}
+
+func generateTasks() []TestItem {
+	return []TestItem{
+		{strings.NewReader(`4
+1 5 7 1
+`), "True"},
+		{strings.NewReader(`3
+2 10 9
+`), "False"},
+	}
+}
