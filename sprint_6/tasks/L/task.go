77aa94f54f6986e94520ef093ef5ee89e7fd2392
Dmitry Burov <dim0n1987@yandex.ru>
2022-07-02 19:37:59 +0300

    update task L for sprint 6

--- sprint_6/tasks/L/task.go
+++ sprint_6/tasks/L/task.go
@@ -1,1 +1,78 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	SUCCESS = "YES"
+	FAIL    = "NO"
+)
+
+type Node struct {
+	value int
+	to    NodeList
+}
+
+type NodeList []*Node
+
+func main() {
+	s := strings.Builder{}
+	Solution(os.Stdin, &s)
+
+	fmt.Println(s.String())
+}
+
+func Solution(r io.Reader, s *strings.Builder) {
+	scanner := bufio.NewScanner(r)
+	scanner.Scan()
+	initData := strings.Fields(scanner.Text())
+
+	vertexes, _ := strconv.Atoi(initData[0])
+	edges, _ := strconv.Atoi(initData[1])
+	list := make(NodeList, vertexes+1)
+
+	for i := 1; i <= vertexes; i++ {
+		list[i] = &Node{
+			value: i,
+			to:    nil,
+		}
+	}
+
+	for i := 0; i < edges; i++ {
+		scanner.Scan()
+		edgeData := strings.Fields(scanner.Text())
+		a, _ := strconv.Atoi(edgeData[0])
+		b, _ := strconv.Atoi(edgeData[1])
+		if a != b {
+			list[a].to = append(list[a].to, list[b])
+			list[b].to = append(list[b].to, list[a])
+		}
+	}
+
+	for _, node := range list {
+		if node == nil {
+			continue
+		}
+		visited := make([]bool, vertexes+1)
+		for _, n := range node.to {
+			visited[n.value] = true
+		}
+		for i, isVisited := range visited {
+			if i == 0 || i == node.value {
+				continue
+			}
+			if !isVisited {
+				s.WriteString(FAIL)
+				return
+			}
+		}
+	}
+
+	s.WriteString(SUCCESS)
+}
--- sprint_6/tasks/L/task_test.go
+++ sprint_6/tasks/L/task_test.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"strings"
+	"testing"
+)
+
+type TestItem struct {
+	input  io.Reader
+	output string
+}
+
+func TestTask(t *testing.T) {
+	tasks := generateTasks()
+
+	for i, v := range tasks {
+		t.Run(fmt.Sprintf("Test %d", i+1), func(t *testing.T) {
+			buf := strings.Builder{}
+			Solution(v.input, &buf)
+			if buf.String() != v.output {
+				t.Errorf("Неверный ответ решения!\nОтвет: \n%v \nВерно: \n%v", buf.String(), v.output)
+			}
+		})
+	}
+}
+
+func generateTasks() []TestItem {
+	return []TestItem{
+		{strings.NewReader(`4 6
+1 2
+2 2
+2 3
+2 4
+3 4
+4 3
+`), "NO"},
+		{strings.NewReader(`3 5
+1 2
+2 1
+3 1
+2 3
+3 3
+`), "YES"},
+	}
+}
