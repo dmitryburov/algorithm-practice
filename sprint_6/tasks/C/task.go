efc54137e0e194ef645bd1363599d1964e8252af
Dmitry Burov <dim0n1987@yandex.ru>
2022-07-03 22:08:30 +0300

    update, not finish!

--- sprint_6/tasks/C/task.go
+++ sprint_6/tasks/C/task.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
+	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
-type Color uint8
+type Color int
 
 const (
 	_ Color = iota
@@ -17,8 +20,8 @@ const (
 )
 
 type Node struct {
+	to    NodeList
 	value int
-	to    []*Node
 }
 
 type NodeList []*Node
@@ -27,9 +30,73 @@ func main() {
 	s := strings.Builder{}
 	Solution(os.Stdin, &s)
 
-	fmt.Println(s.String())
+	fmt.Println(strings.TrimSpace(s.String()))
 }
 
 func Solution(r io.Reader, s *strings.Builder) {
+	scanner := bufio.NewScanner(r)
+	scanner.Scan()
 
+	var (
+		n, m int
+		err  error
+	)
+
+	values := strings.Fields(scanner.Text())
+	n, err = strconv.Atoi(values[0])
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	m, err = strconv.Atoi(values[1])
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	matrix := make(NodeList, n+1)
+	colors := make([]Color, n+1)
+
+	for i := 0; i < m; i++ {
+		scanner.Scan()
+		edge := strings.Fields(scanner.Text())
+
+		a, err := strconv.Atoi(edge[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		b, err := strconv.Atoi(edge[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if matrix[a] == nil {
+			colors[a] = WHITE
+			matrix[a] = &Node{value: a}
+		}
+		if matrix[b] == nil {
+			colors[b] = WHITE
+			matrix[b] = &Node{value: b}
+		}
+		matrix[a].to = append(matrix[a].to, matrix[b])
+		matrix[b].to = append(matrix[b].to, matrix[a])
+	}
+
+	scanner.Scan()
+	start, _ := strconv.Atoi(scanner.Text())
+	DFS(matrix[start], colors, s)
+}
+
+func DFS(node *Node, colors []Color, s *strings.Builder) {
+	if node == nil || colors[node.value] == BLACK {
+		return
+	}
+
+	colors[node.value] = BLACK
+	s.WriteString(fmt.Sprint(node.value) + " ")
+
+	for _, v := range node.to {
+		if colors[v.value] != BLACK {
+			DFS(v, colors, s)
+		}
+	}
 }
--- sprint_6/tasks/C/task_test.go
+++ sprint_6/tasks/C/task_test.go
@@ -1,1 +1,72 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"strings"
+	"testing"
+)
+
+type TestItem struct {
+	input  io.Reader
+	output string
+}
+
+func TestTask(t *testing.T) {
+	tasks := generateTasks()
+
+	for i, v := range tasks {
+		t.Run(fmt.Sprintf("Test %d", i+1), func(t *testing.T) {
+			buf := strings.Builder{}
+			Solution(v.input, &buf)
+			if strings.TrimSpace(buf.String()) != v.output {
+				t.Errorf("Неверный ответ решения!\nОтвет: \n%v \nВерно: \n%v", buf.String(), v.output)
+			}
+		})
+	}
+}
+
+func generateTasks() []TestItem {
+	return []TestItem{
+		{strings.NewReader(`4 4
+3 2
+4 3
+1 4
+1 2
+3
+`), "3 2 1 4"},
+		{strings.NewReader(`2 1
+1 2
+1
+`), "1 2"},
+		{strings.NewReader(`1 0
+1
+`), "1"},
+		{strings.NewReader(`6 7
+3 2
+5 4
+3 1
+1 4
+1 6
+1 2
+1 5
+1
+`), "1 2 3 4 5 6"},
+		{strings.NewReader(`10 13
+9 2
+1 3
+6 10
+5 10
+9 10
+5 9
+3 5
+5 2
+6 7
+5 6
+8 3
+6 1
+4 2
+7
+`), "7 6 1 3 5 2 4 9 10 8"},
+	}
+}
