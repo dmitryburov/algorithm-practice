41c8d29ccabbaed711a72475ae5745296b57456b
Dmitry Burov <dim0n1987@yandex.ru>
2022-07-13 10:54:36 +0300

    fix test for final task B sprint_8

--- sprint_8/final/B/task_test.go
+++ sprint_8/final/B/task_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"strings"
@@ -18,7 +19,8 @@ func TestTask(t *testing.T) {
 	for i, v := range tasks {
 		t.Run(fmt.Sprintf("Test %d", i+1), func(t *testing.T) {
 			buf := strings.Builder{}
-			Solution(v.input, &buf)
+			s, n := initInput(bufio.NewScanner(v.input))
+			Solution(s, n, &buf)
 			if buf.String() != v.output {
 				t.Errorf("Неверный ответ решения!\nОтвет: \n%v \nВерно: \n%v", buf.String(), v.output)
 			}
