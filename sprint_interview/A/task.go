59cffd5b93a0d4823a57917ab4889c290bcb3c81
Dmitry Burov <dim0n1987@yandex.ru>
2022-07-29 13:28:42 +0300

    complete task A for sprint_interview

--- sprint_interview/A/task.go
+++ sprint_interview/A/task.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	a, b := getInputData(bufio.NewScanner(os.Stdin))
+	fmt.Println(solution(a, b))
+}
+
+func solution(a, b int) int {
+	return a + b
+}
+
+func getInputData(scanner *bufio.Scanner) (a, b int) {
+	const bufCapacity = 10000000 // fix long
+	buf := make([]byte, bufCapacity)
+	scanner.Buffer(buf, bufCapacity)
+
+	var err error
+
+	scanner.Scan()
+	a, err = strconv.Atoi(scanner.Text())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b, err = strconv.Atoi(scanner.Text())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return
+}
--- sprint_interview/A/task_test.go
+++ sprint_interview/A/task_test.go
@@ -1,1 +1,44 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+type TestItemInput struct {
+	a, b int
+}
+type TestItem struct {
+	input  struct{ a, b int }
+	output int
+}
+
+func TestTask(t *testing.T) {
+	tasks := generateTasks()
+
+	for i, v := range tasks {
+		t.Run(fmt.Sprintf("Test %d", i+1), func(t *testing.T) {
+			res := solution(v.input.a, v.input.b)
+			if res != v.output {
+				t.Errorf("Неверный ответ решения!\nОтвет: \n%v \nВерно: \n%v", res, v.output)
+			}
+		})
+	}
+}
+
+func generateTasks() []TestItem {
+	return []TestItem{
+		{
+			input:  TestItemInput{a: 12, b: 90},
+			output: 102,
+		},
+		{
+			input:  TestItemInput{a: 200, b: -200},
+			output: 0,
+		},
+		{
+			input:  TestItemInput{a: 1000000000, b: 1000000000},
+			output: 2000000000,
+		},
+	}
+}
